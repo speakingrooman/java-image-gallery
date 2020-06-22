6fd1b61967e1837cca8a238d637f43ee10b15bd0
EC2 Default User <ec2-user@ip-10-0-1-231.us-east-2.compute.internal>
2020-06-22 22:57:08 +0000

    test

--- ec2-scripts/ec2-prod-1.0.sh
+++ ec2-scripts/ec2-prod-1.0.sh
@@ -12,14 +12,26 @@ cd /home/ec2-user
 git clone https://github.com/speakingrooman/java-image-gallery.git
 chown -R ec2-user:ec2-user java-image-gallery
 
+echo "problem is in ec2 scipts"
 CONFIG_BUCKET="s3://edu.au.cc.image-gallery-mohammad-config"
 sudo aws s3 cp ${CONFIG_BUCKET}/nginx/nginx.conf /etc/nginx/nginx.conf
 sudo aws s3 cp ${CONFIG_BUCKET}/nginx/default.d/image_gallery.conf /etc/nginx/default.d/image_gallery.conf
+
+
+
+
 
 # Start/enable services
 systemctl stop postfix
 systemctl disable postfix
 systemctl start nginx
 systemctl enable nginx
+
+
+sudo aws s3 cp s3://edu.au.cc.image-gallery-mohammad-config/nginx/nginx.conf /etc/nginx/nginx.conf
+sudo aws s3 cp s3://edu.au.cc.image-gallery-mohammad-config/nginx/default.d/image_gallery.conf /etc/nginx/default.d/image_gallery.conf
+
+
+
 
 su ec2-user -l -c 'cd ~/java-image-gallery && ./start' >/var/log/image_gallery.log 2>&1 &
